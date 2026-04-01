a29b57b8c139d4f2af0b041e4eb73aa58ce33ff4
Dev`Tar <tatami.non@gmail.com>
2026-04-02 02:08:53 +0700

    Missing files

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -3,9 +3,3 @@ export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080/api/exam'
 };
-
-// # Deploy
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://XXXXXXXXX-api.onrender.com/api/exam'
-// };
